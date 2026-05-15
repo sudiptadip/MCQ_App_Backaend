6ab548a7e3397fbd8185d69f2d7a0704ca1a1887
sudiptadip <sudiptabhattacharjee000@gmail.com>
2026-05-15 08:37:39 +0530

    add

--- MCQAPP/Data/AppDbContext.cs
+++ MCQAPP/Data/AppDbContext.cs
@@ -39,8 +39,16 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=SUDIPTA\\SQLEXPRESS;Database=McqAppDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        // Only configure the context here if no options have been provided by the DI container.
+        // This prevents the hard-coded scaffolded connection string from overriding the
+        // configuration (for example the one in appsettings.json) when AddDbContext is used.
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=SUDIPTA\\SQLEXPRESS;Database=McqAppDb;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
