f7d0915474a63e99e6e8df10399dfdeb7660735f
sudiptadip <sudiptabhattacharjee000@gmail.com>
2026-05-17 15:56:43 +0530

    chage connection string

--- MCQAPP/Program.cs
+++ MCQAPP/Program.cs
@@ -94,11 +94,11 @@ builder.Services.AddScoped<IJwtService, JwtService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseMiddleware<ExceptionMiddleware>();
 
